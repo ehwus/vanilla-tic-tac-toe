22e22c573924278690bc510ccb16990dc60bfc0a
Alex Withington-Smith <alex@awithsmith.com>
2021-04-03 17:16:54 +0100

    round logic now working

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 class Game {
   constructor() {
-    this.currentplayer = 'O';
+    this.currentplayer = 'o';
     this.grid = ['', '', '', '', '', '', '', '', ''];
+    this.addCellEventListeners();
+    this.renderGrid();
   }
 
-  renderCurrentPlayer() {
-    const currentPlayerHTML = document.getElementById('currentPlayer');
-    currentPlayerHTML.innerHTML = `Current Player: ${this.currentplayer}`;
+  playRound(cellIndex) {
+    if (this.grid[cellIndex] != '') return;
+
+    this.grid[cellIndex] = this.currentplayer;
+    if (this.currentplayer === 'o') {
+      this.currentplayer = 'x';
+    } else {
+      this.currentplayer = 'o';
+    }
+    this.renderGrid();
   }
 
   renderGrid() {
@@ -14,9 +23,22 @@ class Game {
       let cellHTML = document.getElementById(`cell-${index}`);
       cellHTML.innerText = cell;
     });
+    this.renderCurrentPlayer();
+  }
+
+  addCellEventListeners() {
+    this.grid.forEach((cell, index) => {
+      let cellHTML = document.getElementById(`cell-${index}`);
+      cellHTML.addEventListener('click', () => {
+        this.playRound(index);
+      });
+    });
+  }
+
+  renderCurrentPlayer() {
+    const currentPlayerHTML = document.getElementById('currentPlayer');
+    currentPlayerHTML.innerHTML = `Current Player: ${this.currentplayer}`;
   }
 }
 
 const game = new Game();
-game.renderCurrentPlayer();
-game.renderGrid();
