fb29f7cff7a3735b8bdc3c2ac3a783f26f01d08c
Alex Withington-Smith <alex@awithsmith.com>
2021-04-03 16:52:09 +0100

    added logic for render grid

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 class Game {
   constructor() {
     this.currentplayer = 'O';
+    this.grid = ['', '', '', '', '', '', '', '', ''];
   }
 
   renderCurrentPlayer() {
     const currentPlayerHTML = document.getElementById('currentPlayer');
     currentPlayerHTML.innerHTML = `Current Player: ${this.currentplayer}`;
   }
+
+  renderGrid() {
+    this.grid.forEach((cell, index) => {
+      let cellHTML = document.getElementById(`cell-${index}`);
+      cellHTML.innerText = cell;
+    });
+  }
 }
 
 const game = new Game();
 game.renderCurrentPlayer();
+game.renderGrid();
