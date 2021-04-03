1f447780a9b702f517dbf356136b9d62830bb002
Alex Withington-Smith <alex@awithsmith.com>
2021-04-03 17:49:59 +0100

    added first two rows of winning logic

--- main.js
+++ main.js
@@ -4,16 +4,17 @@ class Game {
   }
 
   playRound(cellIndex) {
-    if (this.grid[cellIndex] != '') return;
+    if (this._grid[cellIndex] != '') return;
     if (this._winner) return;
 
-    this.grid[cellIndex] = this.currentplayer;
-    if (this.currentplayer === 'o') {
-      this.currentplayer = 'x';
+    this._grid[cellIndex] = this.currentplayer;
+    if (this.currentplayer === 'O') {
+      this.currentplayer = 'X';
     } else {
-      this.currentplayer = 'o';
+      this.currentplayer = 'O';
     }
     this._renderGrid();
+    this._checkForWinner();
   }
 
   reset() {
@@ -23,15 +24,15 @@ class Game {
   }
 
   _renderGrid() {
-    this.grid.forEach((cell, index) => {
+    this._grid.forEach((cell, index) => {
       let cellHTML = document.getElementById(`cell-${index}`);
       cellHTML.innerText = cell;
     });
-    this._renderCurrentPlayer();
+    this._renderCurrentStatus();
   }
 
   _addCellEventListeners() {
-    this.grid.forEach((cell, index) => {
+    this._grid.forEach((cell, index) => {
       let cellHTML = document.getElementById(`cell-${index}`);
       cellHTML.addEventListener('click', () => {
         this.playRound(index);
@@ -39,16 +40,37 @@ class Game {
     });
   }
 
-  _renderCurrentPlayer() {
-    const currentPlayerHTML = document.getElementById('currentPlayer');
-    currentPlayerHTML.innerHTML = `Current Player: ${this.currentplayer}`;
+  _renderCurrentStatus() {
+    const currentStatusHTML = document.getElementById('currentStatus');
+
+    if (this._winner) {
+      currentStatusHTML.innerHTML = `Winner: ${this._winner}! Reset to play again!`;
+    } else {
+      currentStatusHTML.innerHTML = `Current Player: ${this.currentplayer}`;
+    }
   }
 
-  _checkForWinner() {}
+  _checkForWinner() {
+    const grid = this._grid;
+    if (grid[0] === grid[1] && grid[1] === grid[2]) {
+      if (grid[0] != '' && grid[1] != '' && grid[2] != '') {
+        this._winner = grid[0];
+        this._renderCurrentStatus();
+        return;
+      }
+    }
+    if (grid[3] === grid[4] && grid[4] === grid[5]) {
+      if (grid[3] != '' && grid[4] != '' && grid[5] != '') {
+        this._winner = grid[3];
+        this._renderCurrentStatus();
+        return;
+      }
+    }
+  }
 
   _startGame() {
-    this.currentplayer = 'o';
-    this.grid = ['', '', '', '', '', '', '', '', ''];
+    this.currentplayer = 'O';
+    this._grid = ['', '', '', '', '', '', '', '', ''];
     this._addCellEventListeners();
     this._renderGrid();
     this._winner = false;
